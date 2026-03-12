8425f19602cbc62e32b937205605c4e8736f352f
Pedro Santos <pedro.posantos7@gmail.com>
2026-03-12 19:27:04 +0000

    lab2

--- ula_modules.py
+++ ula_modules.py
@@ -26,8 +26,8 @@ def halfAdder(a, b, soma, carry):
     """
     @always_comb
     def comb():
-        pass
-
+        soma.next = a ^ b
+        carry.next = a & b
     return instances()
 
 
@@ -42,10 +42,31 @@ def fullAdder(a, b, c, soma, carry):
         soma: Saida de soma.
         carry: Carry de saida.
     """
+    #apenas para a solução com dois half adder
+    #s1 = Signal(bool(0)) # (1)
+    #s2 = Signal(bool(0)) 
+    #s3 = Signal(bool(0))
+
+    #uma forma de melhorar é ao invés de escrever os sinais dessa forma, colocar num vetor:
+    s = [Signal(bool(0)) for i in range(3)]
+
+    #half_1 = halfAdder(a, b, s[0], s[1]) 
+    #half_2 = halfAdder(c, s[0], soma, s[2])
+
+    #vetor
+    haList = [None for i in range(2)]  # (1)
+
+    haList[0] = halfAdder(a, b, s[0], s[1]) 
+    haList[1] = halfAdder(c, s[0], soma, s[2])
+
     @always_comb
     def comb():
-        pass
+        #solução padrão
+        #carry.next = (a & b) or (a & c) or (c & b)
+        #soma.next = ((not a) & (not b) & c) or ((not a) & b & (not c)) or (a & (not b) & (not c)) or (a & b & c)
 
+        #solução com dois halfAdder
+        carry.next = s[1] | s[2]
     return instances()
 
 
@@ -62,6 +83,12 @@ def adder2bits(x, y, soma, carry):
         soma: Vetor de saida de 2 bits.
         carry: Carry de saida.
     """
+    c_int = Signal(bool(0))
+
+    full1 = fullAdder(x[0], y[0], 0, soma[0], c_int)
+
+    full2 = fullAdder(x[1], y[1], c_int, soma[1], carry)
+
     return instances()
 
 
@@ -78,6 +105,22 @@ def adder(x, y, soma, carry):
         soma: Vetor de saida com mesma largura de x/y.
         carry: Carry de saida mais significativo.
     """
+    n = len(x)
+    faList = [None for i in range(n)]
+
+    carry_int_list = [Signal(bool(0)) for j in range(n)]
+
+
+
+    for k in range(n):
+        if k == 0:
+            c_in = Signal(bool(0))
+        else:
+            c_in = carry_int_list[k-1]
+        
+        c_out = carry_int_list[k]
+
+        faList[k] = fullAdder(x[k], y[k], c_in, soma[k], c_out)
     return instances()
 
 
@@ -94,8 +137,13 @@ def addervb(x, y, soma, carry):
         soma: Vetor de saida.
         carry: Carry de saida.
     """
+    n = len(x)
+
     @always_comb
     def comb():
-        pass
+        result = intbv(x + y)[n+1:] 
+
+        soma.next = result[n:0]
+        carry.next = result[n]
 
     return instances()
